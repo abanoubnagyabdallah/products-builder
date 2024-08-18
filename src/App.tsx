f4d8dd036795f79f9a2d32e38eee0c3a2f9e8400
Abanoub Nagy <abanoubnagy60@gmail.com>
2024-08-18 17:54:13 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import ProductCard from "./components/ProductCard";
 
 function App() {
   
   return (
     <>
-      
+      <div className="border-2 border-red-500 m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-indigo-500 p-2 gap-2 rounded-md">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
+
+// sm = md = lg = xl = 2xl = 3xl
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,7 @@
+
+interface IProps{}
+
+export default function ProductCard({}: IProps) {
+  return <div className="border border-white text-white rounded-md p-3 text-sm">ProductCard</div>;
+}
+
